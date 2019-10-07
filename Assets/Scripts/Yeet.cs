a50f7fb353b0f54f5d2d6d8d19127c3a33d896bf
Andrew Colannino <akc8012@gmail.com>
2019-10-07 15:17:31 -0400

    got our boi json and it works!!!

--- Assets/Scripts/Yeet.cs
+++ Assets/Scripts/Yeet.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 public class Yeet : MonoBehaviour
 {
 	void Awake()
 	{
 		Debug.Log(typeof(string).Assembly.ImageRuntimeVersion);
+
+		var json = JObject.Parse("{\"message\": \"waddup\"}");
+		Debug.Log(json["message"].Value<string>());
 	}
 }
